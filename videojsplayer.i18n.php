2460d16489aafd4290a438a47b1770213cbb3222
Aalekh Nigam <aalekh1993@rediffmail.com>
2014-01-01 13:16:40 +0530

    Create videojsplayer.i18n.php
    Created Internalization File

---
+++ videojsplayer.i18n.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Messages file for the VideoJsPlayer extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+/**
+ * Get all extension messages
+ *
+ * @return array
+ */
+
+$message=array();
+
+$messages['en'] = array(
+  'videojsplayer-desc' => 'Allow inclusion of videojs player lets play video of mp4,webm and ogv type',
+  'videojsplayer-error-no-type' => 'No video with supported format and MIME type found',
+ 'videojsplayer-imp-parameters' => 'Please specify "sourcemp4","sourceogv","sourcewebm","height","width" for extension to work properly',
+ 'sourcemp4' => 'specify source of the video of mp4 type',
+ 'sourcewebm' => 'specify source of the video of webm type',
+ 'sourceogv' => 'specify source of the video of ogv type',
+ 'height' => 'specify height of the media player',
+'width' => 'specify width of the media player'
+);
